6b320445df510714d980bf0a33bcb0b177c89cda
Minus <zsh.dev@outlook.com>
2020-07-19 19:58:07 +0800

    20200719 delete shell scripts

--- shell_scripts/ex19b.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# locate -- 在locate的数据库中查找指定的样式
-
-locatedb="/tmp/locate.db"
-
-exec grep -i "$@" $locatedb
--- shell_scripts/ex56.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# zipcode -- 根据邮政编码，识别出对应的城市和州。
-# 数据取自city-data.com，在该站点中，每个邮政编码都有自己的页面。
-
-baseURL="http://www.city-data.com/zips"
-
-/bin/echo -n "ZIP code $1 is in "
-
-curl -s -dump "$baseURL/$1.xhtml" | grep -i '<title>' | cut -d\( -f2 | cut -d\) -f1
